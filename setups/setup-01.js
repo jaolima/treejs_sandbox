2bac829d6ada6b614532aff2d58658aa546ea4c4
Joao Fonseca <joao.fonseca@hex360.com.br>
2024-02-15 22:58:53 -0300

    [First steps]: Add Renderer

--- setups/setup-01.js
+++ setups/setup-01.js
@@ -1,1 +1,18 @@
-console.log(THREE)
+const options = {
+    targetSelector: '#scene', //Propriedade que diz onde o nosso render ira desenhar as coisas
+    width: 800, height: 600, // Tamanho do nosso render
+    backgroundColor: 0x222222 // Cor
+}
+
+// Renderer displays your beautifully crafted scenes using WebGL.
+const renderer = new THREE.WebGLRenderer();
+
+// Set renderer sizing
+renderer.setSize(
+    options.width, options.height
+)
+
+// Add Element in the DOM
+document.querySelector(
+    options.targetSelector
+).appendChild(renderer.domElement);
