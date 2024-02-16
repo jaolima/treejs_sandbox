ae858ea55044a8f6cf6422047ec6321fa1716b91
Joao Fonseca <joao.fonseca@hex360.com.br>
2024-02-16 19:20:33 -0300

    [Geometric and Shapes]: Cylinder, Plane and Spheres.

--- scenes/scene-02.js
+++ scenes/scene-02.js
@@ -4,6 +4,7 @@ const material = new THREE.MeshLambertMaterial(
     { color: 0x348feb }
 );
 
+// CUBE
 const cube = new THREE.Mesh(
     new THREE.BoxBufferGeometry(
         1, 1, 1
@@ -13,6 +14,7 @@ const cube = new THREE.Mesh(
 
 scene.add(cube);
 
+// CIRCLE
 const circle = new THREE.Mesh(
     // radius, segments
     new THREE.CircleBufferGeometry(
@@ -25,6 +27,7 @@ circle.position.x = -2;
 circle.rotation.x =  THREE.MathUtils.degToRad(-90);
 scene.add(circle);
 
+//  CONE
 const cone = new THREE.Mesh(
     // radius, height, segments
 new THREE.ConeBufferGeometry(
@@ -38,9 +41,49 @@ cone.position.y =  2;
 scene.add(cone);
         
 
-        
+// CYLINDER
+const cylinder = new THREE.Mesh(
+    // radiusTop, radiusBottom, height, radialSegments
+new THREE.CylinderBufferGeometry(
+        0.5, 0.5, 1
+),
+        material
+);
+cylinder.position.x = 2;
+cylinder.position.y =  0;
+
+scene.add(cylinder);
+
+// PLANE
+const plane = new THREE.Mesh(
+    // radiusTop, radiusBottom, height, radialSegments
+new THREE.PlaneBufferGeometry(
+        0.5, 0.5, 1
+),
+        material
+);
+plane.position.x = 2;
+plane.position.y =  2;
+plane.rotation.x =  THREE.MathUtils.degToRad(-90);
+
+scene.add(plane);
+
+// SPHERE
+const sphere = new THREE.Mesh(
+    // radiusTop, widthSegment, heightSegments
+new THREE.SphereBufferGeometry(
+        0.3, 20, 20
+),
+        material
+);
+sphere.position.x = 0;
+sphere.position.y =  2;
+
+scene.add(sphere);
 x3.add(cone, { label: 'cone' });
 x3.add(circle, { label: 'circle' });
+x3.add(cylinder, { label: 'cylinder' });
+x3.add(plane, { label: 'plane' });
 
 // takes the scene and the camera and draws the scene (like a photograph)
 /*
