64382eb7630b5946f2e2dc13e292c6997a0c3a74
Joao Fonseca <joao.fonseca@hex360.com.br>
2024-02-18 23:41:17 -0300

    [Geometric and Design] Draw Romantic Heart

--- scenes/scene-03.js
+++ scenes/scene-03.js
@@ -1,8 +1,16 @@
 const path = new THREE.Path();
 
 // where drawing passes through
-path.moveTo(1, 3);
-path.quadraticCurveTo(5, 1, 8, 3);
+// Romantic Heart
+path.moveTo(0.3, 1.5);
+path.quadraticCurveTo(0.3, 2.2, 0.9, 2.2);
+path.quadraticCurveTo(1.3, 2.2, 1.4, 1.7);
+path.quadraticCurveTo(1.5, 2.2, 1.9, 2.2);
+path.quadraticCurveTo(2.5, 2.2, 2.5, 1.5);
+path.quadraticCurveTo(2.5, 1.0, 1.4, 0.3);
+path.quadraticCurveTo(0.3, 1.0, 0.3, 1.5);
+// path.quadraticCurveTo(5, 1, 8, 3);
+// path.bezierCurveTo(3, 5, 6, 1, 8, 3);
 
 // path.lineTo(3, 3);
 // path.lineTo(3, 1);
